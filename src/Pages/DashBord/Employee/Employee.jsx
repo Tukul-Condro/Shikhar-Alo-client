2b179f5631faeb1dd59e8ba5b521f7a3ff69c2c6
tukulcondro <tukuldas68@gmail.com>
2026-02-28 03:43:11 +0600

    employee update

--- src/Pages/DashBord/Employee/Employee.jsx
+++ src/Pages/DashBord/Employee/Employee.jsx
@@ -5,7 +5,7 @@ import UpdateWork from './UpdateWork'
 import useAuth from '../../../Hooks/useAuth';
 import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 import Swal from 'sweetalert2';
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import useWorks from '../../../Hooks/useWorks';
 
 const Employee = () => {
@@ -14,49 +14,6 @@ const Employee = () => {
     const axiosSecure = useAxiosSecure(); 
     const  [, refetch] = useWorks();
     const [editWork, setEditWork] = useState(null);
-
-    // handle form submit
-    // const handleWorkSubmit = async (data) =>{
-    // console.log("Data from child:", data);
-    //     // work data from addworkFrom
-    //    const workData = {
-    //     task: data.task,
-    //     workHour: data.workHour,
-    //     date: data.date,
-    //     name: user?.displayName,
-    //     email: user?.email,
-    //    }
-    //    console.log('work data', workData)
-    // //    worker info
-    // //    const worker = {
-    // //     name: user?.displayName,
-    // //     email: user?.email,
-    // //    }
-    // //    console.log(worker)
-
-    //    const AddWorkData = {
-    //     workData,
-    //     // worker
-    //    }
-    //    console.table(AddWorkData)
-
-    // // Add new work to state and sort by date descending
-    // setWorks(prev =>{
-    //     const update = [workData,...(prev|| [])].sort((a, b) => new Date(b.date) - new Date(a.date));
-    //     return update;
-    // })
-    
-
-    // //    save work in DB
-    // try{
-    //     // post req
-    //     await AxiosSecure.post(`/addWorks/${user.email}`,AddWorkData)
-        
-    //     Swal.fire("Work Add Successfuly!");
-    // }catch (err){
-    //     console.log(err)
-    // }
-    // }
 
     // Add new work
     const handleWorkSubmit = async (data)=>{
@@ -74,8 +31,6 @@ const Employee = () => {
 
             if(res.data.insertedId){
                 Swal.fire("Work Added Successfully");
-                // const work = await AxiosSecure.get(`/works/${user.email}`);
-                // setWorks(works.data);
                 refetch();
             }
         }catch(err){
